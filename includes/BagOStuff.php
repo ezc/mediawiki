ezc/mediawiki

commit 77db29c69a4eb1d23834c849d60106aca6f0f1fb

Removed most exit() calls from the MediaWiki core, by replacing them with either a throw or by classic error checking. OutputPage::fatalError() and similar functions are deprecated, use either OutputPage::showFatalError() to return control or throw new FatalError() to relinquish it. Backwards compatibility of all interfaces has been maintained, thus extensions should continue to work and can be ported at a later date.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@14631 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/BagOStuff.php
@@ -35,7 +35,7 @@
  * @package MediaWiki
  * @abstract
  */
-class BagOStuff {
+abstract class BagOStuff {
 	var $debugmode;
 
 	function BagOStuff() {
@@ -212,7 +212,7 @@ CREATE TABLE objectcache (
  * @abstract
  * @package MediaWiki
  */
-class SqlBagOStuff extends BagOStuff {
+abstract class SqlBagOStuff extends BagOStuff {
 	var $table;
 	var $lastexpireall = 0;
 
@@ -292,16 +292,9 @@ class SqlBagOStuff extends BagOStuff {
 	function _blobencode($str) {
 		return $str;
 	}
-	function _doinsert($table, $vals) {
-		wfDie( 'abstract function SqlBagOStuff::_doinsert() must be defined' );
-	}
-	function _doquery($sql) {
-		wfDie( 'abstract function SqlBagOStuff::_doquery() must be defined' );
-	}
 
-	function _fetchrow($res) {
-		wfDie( 'abstract function SqlBagOStuff::_fetchrow() must be defined' );
-	}
+	abstract function _doinsert($table, $vals);
+	abstract function _doquery($sql);
 
 	function _freeresult($result) {
 		/* stub */
@@ -313,13 +306,8 @@ class SqlBagOStuff extends BagOStuff {
 		return 'unknown error';
 	}
 
-	function _maxdatetime() {
-		wfDie( 'abstract function SqlBagOStuff::_maxdatetime() must be defined' );
-	}
-
-	function _fromunixtime() {
-		wfDie( 'abstract function SqlBagOStuff::_fromunixtime() must be defined' );
-	}
+	abstract function _maxdatetime();
+	abstract function _fromunixtime($ts);
 
 	function garbageCollect() {
 		/* Ignore 99% of requests */
@@ -427,7 +415,7 @@ class MediaWikiBagOStuff extends SqlBagOStuff {
 			/* This is actually a hack, we should be able
 			   to use Language classes here... or not */
 			if (!$dbw)
-				wfDie("Could not connect to database");
+				throw new MWException("Could not connect to database");
 			$this->table = $dbw->tableName( $this->table );
 			$this->tableInitialised = true;
 		}

includes/Article.php
@@ -1464,7 +1464,7 @@ class Article {
 			$wgOut->returnToMain( false, $rcTitle->getPrefixedText() );
 		}
 		else {
-			$wgOut->errorpage( 'markedaspatrollederror', 'markedaspatrollederrortext' );
+			$wgOut->showErrorPage( 'markedaspatrollederror', 'markedaspatrollederrortext' );
 		}
 	}
 
@@ -1477,7 +1477,7 @@ class Article {
 		global $wgUser, $wgOut;
 
 		if ( $wgUser->isAnon() ) {
-			$wgOut->errorpage( 'watchnologin', 'watchnologintext' );
+			$wgOut->showErrorPage( 'watchnologin', 'watchnologintext' );
 			return;
 		}
 		if ( wfReadOnly() ) {
@@ -1525,7 +1525,7 @@ class Article {
 		global $wgUser, $wgOut;
 
 		if ( $wgUser->isAnon() ) {
-			$wgOut->errorpage( 'watchnologin', 'watchnologintext' );
+			$wgOut->showErrorPage( 'watchnologin', 'watchnologintext' );
 			return;
 		}
 		if ( wfReadOnly() ) {
@@ -1658,7 +1658,7 @@ class Article {
 	 */
 	function flattenRestrictions( $limit ) {
 		if( !is_array( $limit ) ) {
-			wfDebugDieBacktrace( 'Article::flattenRestrictions given non-array restriction set' );
+			throw new MWException( 'Article::flattenRestrictions given non-array restriction set' );
 		}
 		$bits = array();
 		ksort( $limit );
@@ -1705,7 +1705,7 @@ class Article {
 		$conds = $this->mTitle->pageCond();
 		$latest = $dbw->selectField( 'page', 'page_latest', $conds, $fname );
 		if ( $latest === false ) {
-			$wgOut->fatalError( wfMsg( 'cannotdelete' ) );
+			$wgOut->showFatalError( wfMsg( 'cannotdelete' ) );
 			return;
 		}
 
@@ -1899,7 +1899,7 @@ class Article {
 				$wgOut->returnToMain( false );
 				wfRunHooks('ArticleDeleteComplete', array(&$this, &$wgUser, $reason));
 			} else {
-				$wgOut->fatalError( wfMsg( 'cannotdelete' ) );
+				$wgOut->showFatalError( wfMsg( 'cannotdelete' ) );
 			}
 		}
 	}
@@ -2522,7 +2522,7 @@ class Article {
 		$fname = 'Article::info';
 
 		if ( !$wgAllowPageInfo ) {
-			$wgOut->errorpage( 'nosuchaction', 'nosuchactiontext' );
+			$wgOut->showErrorPage( 'nosuchaction', 'nosuchactiontext' );
 			return;
 		}
 

includes/DatabaseFunctions.php
@@ -18,7 +18,7 @@ function wfQuery( $sql, $db, $fname = '' ) {
 	global $wgOut;
 	if ( !is_numeric( $db ) ) {
 		# Someone has tried to call this the old way
-		$wgOut->fatalError( wfMsgNoDB( 'wrong_wfQuery_params', $db, $sql ) );
+		throw new FatalError( wfMsgNoDB( 'wrong_wfQuery_params', $db, $sql ) );
 	}
 	$c =& wfGetDB( $db );
 	if ( $c !== false ) {

includes/Exception.php
@@ -110,6 +110,25 @@ class FatalError extends MWException {
 	}
 }
 
+class ErrorPageError extends MWException {
+	public $title, $msg;
+	
+	/**
+	 * Note: these arguments are keys into wfMsg(), not text!
+	 */
+	function __construct( $title, $msg ) {
+		$this->title = $title;
+		$this->msg = $msg;
+		parent::__construct( wfMsg( $msg ) );
+	}
+
+	function report() {
+		global $wgOut;
+		$wgOut->showErrorPage( $this->title, $this->msg );
+		$wgOut->output();
+	}
+}
+
 /**
  * Install an exception handler for MediaWiki exception types.
  */
@@ -150,20 +169,22 @@ function wfReportException( Exception $e ) {
  *   try {
  *       ...
  *   } catch ( MWException $e ) {
- * 
  *       $e->report();
  *   } catch ( Exception $e ) {
  *       echo $e->__toString();
  *   }
  */
 function wfExceptionHandler( $e ) {
+	global $wgFullyInitialised;
 	wfReportException( $e );
 	
 	// Final cleanup, similar to wfErrorExit()
-	try {
-		wfProfileClose();
-		logProfilingData();
-	} catch ( Exception $e ) {}
+	if ( $wgFullyInitialised ) {
+		try {
+			wfProfileClose();
+			logProfilingData(); // uses $wgRequest, hence the $wgFullyInitialised condition
+		} catch ( Exception $e ) {}
+	}
 
 	// Exit value should be nonzero for the benefit of shell jobs
 	exit( 1 );

includes/Export.php
@@ -634,7 +634,7 @@ class DumpNamespaceFilter extends DumpFilter {
 				$ns = intval( $key );
 				$this->namespaces[$ns] = true;
 			} else {
-				wfDie( "Unrecognized namespace key '$key'\n" );
+				throw new MWException( "Unrecognized namespace key '$key'\n" );
 			}
 		}
 	}

includes/FakeTitle.php
@@ -4,7 +4,7 @@
  * Fake title class that triggers an error if any members are called
  */
 class FakeTitle {
-	function error() { wfDebugDieBacktrace( "Attempt to call member function of FakeTitle\n" ); }
+	function error() { throw new MWException( "Attempt to call member function of FakeTitle\n" ); }
 
 	// PHP 5.1 method overload
 	function __call( $name, $args ) { $this->error(); }

includes/GlobalFunctions.php
@@ -642,7 +642,7 @@ function wfErrorExit() {
  */
 function wfDie( $msg='' ) {
 	echo $msg;
-	die( -1 );
+	die( 1 );
 }
 
 /**
@@ -665,7 +665,7 @@ function wfDebugDieBacktrace( $msg = '' ) {
 	}
 	echo $msg;
 	echo wfReportTime()."\n";
-	die( -1 );
+	die( 1 );
 }
 
 /**
@@ -1364,7 +1364,7 @@ function wfTimestamp($outputtype=TS_UNIX,$ts=0) {
 		case TS_ORACLE:
 			return gmdate( 'd-M-y h.i.s A', $uts) . ' +00:00';
 		default:
-			wfDebugDieBacktrace( 'wfTimestamp() called with illegal output type.');
+			throw new MWException( 'wfTimestamp() called with illegal output type.');
 	}
 }
 
@@ -1605,7 +1605,7 @@ function wfEncryptPassword( $userid, $password ) {
  */
 function wfAppendToArrayIfNotDefault( $key, $value, $default, &$changed ) {
 	if ( is_null( $changed ) ) {
-		wfDebugDieBacktrace('GlobalFunctions::wfAppendToArrayIfNotDefault got null');
+		throw new MWException('GlobalFunctions::wfAppendToArrayIfNotDefault got null');
 	}
 	if ( $default[$key] !== $value ) {
 		$changed[$key] = $value;
@@ -1726,7 +1726,7 @@ function wfUsePHP( $req_ver ) {
 	$php_ver = PHP_VERSION;
 
 	if ( version_compare( $php_ver, (string)$req_ver, '<' ) )
-		 wfDebugDieBacktrace( "PHP $req_ver required--this is only $php_ver" );
+		 throw new MWException( "PHP $req_ver required--this is only $php_ver" );
 }
 
 /**
@@ -1746,7 +1746,7 @@ function wfUseMW( $req_ver ) {
 	global $wgVersion;
 
 	if ( version_compare( $wgVersion, (string)$req_ver, '<' ) )
-		wfDebugDieBacktrace( "MediaWiki $req_ver required--this is only $wgVersion" );
+		throw new MWException( "MediaWiki $req_ver required--this is only $wgVersion" );
 }
 
 /**

includes/Group.php
@@ -90,7 +90,7 @@ class Group {
 	/** Initialise a new row in the database */
 	function addToDatabase() {
 		if ( Group::getStaticGroups() ) {
-			wfDebugDieBacktrace( "Can't modify groups in static mode" );
+			throw new MWException( "Can't modify groups in static mode" );
 		}
 
 		$fname = 'Group::addToDatabase';
@@ -110,7 +110,7 @@ class Group {
 		global $wgMemc;
 
 		if ( Group::getStaticGroups() ) {
-			wfDebugDieBacktrace( "Can't modify groups in static mode" );
+			throw new MWException( "Can't modify groups in static mode" );
 		}
 		if($this->id == 0) { return; }
 
@@ -136,7 +136,7 @@ class Group {
 		global $wgMemc;
 
 		if ( Group::getStaticGroups() ) {
-			wfDebugDieBacktrace( "Can't modify groups in static mode" );
+			throw new MWException( "Can't modify groups in static mode" );
 		}
 		if($this->id == 0) { return; }
 

commit 1e29f5ccf193ec10482e3b58d539b66f655d038a

Unbreak the wiki when the base BagOStuff class is used for faux cache objects (as in parser tests) This was broken in r14631 by declaring the class abstract, and therefore non-instantiable.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@14632 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/BagOStuff.php
@@ -33,9 +33,8 @@
  * $bag = new MysqlBagOStuff($tablename); # connect to db first
  *
  * @package MediaWiki
- * @abstract
  */
-abstract class BagOStuff {
+class BagOStuff {
 	var $debugmode;
 
 	function BagOStuff() {