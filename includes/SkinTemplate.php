ezc/mediawiki

commit 21d9355bcb488a799887b6fb4e7bc0244e29aa06

If the page doesn't exist and the user can't create it, omit the "edit"/"view source" tab entirely (since the user can't edit and there's no source to view).
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@26189 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/SkinTemplate.php
@@ -685,7 +685,7 @@ class SkinTemplate extends Skin {
 						'href' => $this->mTitle->getLocalUrl( 'action=edit&section=new' )
 					);
 				}
-			} else {
+			} elseif ( $this->mTitle->exists() ) {
 				$content_actions['viewsource'] = array(
 					'class' => ($action == 'edit') ? 'selected' : false,
 					'text' => wfMsg('viewsource'),

RELEASE-NOTES
@@ -30,6 +30,8 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * (bug 5412) Add feed links for the site to all pages
 * (bug 11353) Add ability to retrieve raw section content via action=raw
 * Show relevant deletion log lines when uploading a previously deleted file
+* On SkinTemplate based skins (like MonoBook), omit confusing "edit"/"view source"
+  tab entirely if the page doesn't exist and the user isn't allowed to create it
 
 === Bug fixes in 1.12 ===
 

commit 0b744b7919aaacd5d937e06d201443edb3d77f91

fix minor regression in r26189: users should be able to view the source of default system messages
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@26190 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/SkinTemplate.php
@@ -685,7 +685,7 @@ class SkinTemplate extends Skin {
 						'href' => $this->mTitle->getLocalUrl( 'action=edit&section=new' )
 					);
 				}
-			} elseif ( $this->mTitle->exists() ) {
+			} elseif ( $this->mTitle->exists() || $this->mTitle->isAlwaysKnown() ) {
 				$content_actions['viewsource'] = array(
 					'class' => ($action == 'edit') ? 'selected' : false,
 					'text' => wfMsg('viewsource'),