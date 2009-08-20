ezc/mediawiki

commit e68f2f301ff078a30dbe5798b95e3b2e98ed3470

Save the variant find from HTTP_ACCEPT_LANGUAGE to $mPreferredVariant, so we do not need to do it again.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@55371 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

languages/LanguageConverter.php
@@ -200,15 +200,15 @@ class LanguageConverter {
 					// strip whitespace
 					$language = trim( $language );
 					if( in_array( $language, $this->mVariants ) ) {
-						return $language;
-						break;
+						$this->mPreferredVariant = $language;
+						return $this->mPreferredVariant;
 					}
 				}
 			}
 		}
 
-		return $this->mMainLanguageCode;
-
+		$this->mPreferredVariant = $this->mMainLanguageCode;
+		return $this->mPreferredVariant;
 	}
 	
 	/**

commit 10622a687d80b8e9ea751b03ca0ed6587e2e6bee

Fixed some of tests failed caused by r55371.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@55374 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

languages/LanguageConverter.php
@@ -207,8 +207,7 @@ class LanguageConverter {
 			}
 		}
 
-		$this->mPreferredVariant = $this->mMainLanguageCode;
-		return $this->mPreferredVariant;
+		return $this->mMainLanguageCode;
 	}
 	
 	/**