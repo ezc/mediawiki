ezc/mediawiki

commit 660a8f3c558cf7e98668229070561e7141c25906

Creating new function wgOutput->showLogs and including new information on viewing non-existant user pages. In detail: Instead of copy&pasting the code in different files, there is now one function for showing logs. This function is currently used for: * Article and EditPage: Show deletion / move log * Article: Show rename log on user(talk)pages (NEW in this revision) * SpecialContributions: Show block log for blocked users (Note: I removed the condition "log_action != 'revision'". AFAIK it isn't needed, log lists are checked for permission somewhere)
Additionally, on user (talk) pages a note is displayed, if the user does not exist.


git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@56251 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/Article.php
@@ -1200,8 +1200,23 @@ class Article {
 	 */
 	public function showMissingArticle() {
 		global $wgOut, $wgRequest, $wgUser;
+
+		# Show info in user (talk) namespace. Does the user exist and if not, has he been renamed.
+		if ( $this->mTitle->getNamespace() == NS_USER || $this->mTitle->getNamespace() == NS_USER_TALK ) {
+			$id = User::idFromName( $this->mTitle->getBaseText() );
+			$ip = User::isIP( $this->mTitle->getBaseText() );
+			if ( $id == 0 && !$ip ) { # User does not exist
+				$wgOut->wrapWikiMsg( '<div class="mw-userpage-userdoesnotexist error">$1</div>',
+					array( 'userpage-userdoesnotexist-view', $this->mTitle->getBaseText() ) );
+
+				# Show rename log because user does not exist. 
+				$parent = $this->mTitle->getNsText() . ":" . $this->mTitle->getBaseText();
+				$wgOut->showLogs( $parent, '', array( 'renameuser' ), 'renamed-notice' );
+			}
+
+		}
 		# Show delete and move logs
-		$this->showLogs();
+		$wgOut->showLogs( $this->mTitle->getPrefixedText(), '', array( 'delete', 'move' ), 'moveddeleted-notice' );
 
 		# Show error message
 		$oldid = $this->getOldID();
@@ -1263,36 +1278,6 @@ class Article {
 				'rev-suppressed-text-view' : 'rev-deleted-text-view';
 			$wgOut->wrapWikiMsg( "<div class='mw-warning plainlinks'>\n$1</div>\n", $msg );
 			return true;
-		}
-	}
-
-	/**
-	 * Show an excerpt from the deletion and move logs. To be called from the 
-	 * header section on page views of missing pages.
-	 */
-	public function showLogs() {
-		global $wgUser, $wgOut;
-		$loglist = new LogEventsList( $wgUser->getSkin(), $wgOut );
-		$pager = new LogPager( $loglist, array('move', 'delete'), false,
-			$this->mTitle->getPrefixedText(), '', array( "log_action != 'revision'" ) );
-		if( $pager->getNumRows() > 0 ) {
-			$pager->mLimit = 10;
-			$wgOut->addHTML( '<div class="mw-warning-with-logexcerpt">' );
-			$wgOut->addWikiMsg( 'moveddeleted-notice' );
-			$wgOut->addHTML(
-				$loglist->beginLogEventsList() .
-				$pager->getBody() .
-				$loglist->endLogEventsList()
-			);
-			if( $pager->getNumRows() > 10 ) {
-				$wgOut->addHTML( $wgUser->getSkin()->link(
-					SpecialPage::getTitleFor( 'Log' ),
-					wfMsgHtml( 'log-fulllog' ),
-					array(),
-					array( 'page' => $this->mTitle->getPrefixedText() )
-				) );
-			}
-			$wgOut->addHTML( '</div>' );
 		}
 	}
 

includes/EditPage.php
@@ -730,7 +730,7 @@ class EditPage {
 		}
 		# Give a notice if the user is editing a deleted/moved page...
 		if ( !$this->mTitle->exists() ) {
-			$this->showLogs( $wgOut );
+			$wgOut->showLogs( $this->mTitle->getPrefixedText(), '', array( 'delete', 'move' ), 'recreate-moveddeleted-warn' );
 		}
 	}
 
@@ -2479,42 +2479,6 @@ END
 		global $wgOut;
 		$wgOut->setPageTitle( wfMsg( 'nocreatetitle' ) );
 		$wgOut->addWikiMsg( 'nocreatetext' );
-	}
-
-	/**
-	 * If there are rows in the deletion/move log for this page, show them,
-	 * along with a nice little note for the user
-	 *
-	 * @param OutputPage $out
-	 */
-	protected function showLogs( $out ) {
-		global $wgUser;
-		$loglist = new LogEventsList( $wgUser->getSkin(), $out );
-		$pager = new LogPager( $loglist, array('move', 'delete'), false,
-			$this->mTitle->getPrefixedText(), '', array( "log_action != 'revision'" ) );
-
-		$count = $pager->getNumRows();
-		if ( $count > 0 ) {
-			$pager->mLimit = 10;
-			$out->addHTML( '<div class="mw-warning-with-logexcerpt">' );
-			$out->addWikiMsg( 'recreate-moveddeleted-warn' );
-			$out->addHTML(
-				$loglist->beginLogEventsList() .
-				$pager->getBody() .
-				$loglist->endLogEventsList()
-			);
-			if($count > 10){
-				$out->addHTML( $wgUser->getSkin()->link(
-					SpecialPage::getTitleFor( 'Log' ),
-					wfMsgHtml( 'log-fulllog' ),
-					array(),
-					array( 'page' => $this->mTitle->getPrefixedText() ) ) );
-			}
-			$out->addHTML( '</div>' );
-			return true;
-		}
-		
-		return false;
 	}
 
 	/**

includes/specials/SpecialContributions.php
@@ -147,7 +147,7 @@ class SpecialContributions extends SpecialPage {
 	* @return String: appropriately-escaped HTML to be output literally
 	*/
 	protected function contributionsSub( $nt, $id ) {
-		global $wgSysopUserBans, $wgLang, $wgUser;
+		global $wgSysopUserBans, $wgLang, $wgUser, $wgOut;
 
 		$sk = $wgUser->getSkin();
 
@@ -223,7 +223,11 @@ class SpecialContributions extends SpecialPage {
 			wfRunHooks( 'ContributionsToolLinks', array( $id, $nt, &$tools ) );
 	
 			$links = $wgLang->pipeList( $tools );
-			$this->showBlock( $nt, $id );
+
+			// Show a note if the user is blocked and display the last block log entry.
+			if ( User::newFromID( $id )->isBlocked() )
+				$wgOut->showLogs( $nt->getPrefixedText(), '', array( 'block' ), 
+						'sp-contributions-blocked-notice', 1 );
 		}
 	
 		// Old message 'contribsub' had one parameter, but that doesn't work for
@@ -237,40 +241,6 @@ class SpecialContributions extends SpecialPage {
 		}
 	}
 
-	/**
-	 * Show a note if the user is blocked and display the last block log entry.
-	 * @param Title $title Title object for the target
-	 * @param $userId ID of the user
-	 */
-	protected function showBlock( $title, $userId ) {
-		global  $wgUser, $wgOut;
-		if ( !User::newFromID( $userId )->isBlocked() )
-			return; # User is not blocked, nothing to do here
-		$loglist = new LogEventsList( $wgUser->getSkin(), $wgOut );
-		$pager = new LogPager( $loglist, 'block', false, $title->getPrefixedText() );
-		// Check if there is something in the block log.
-		// If this is not the case, either the user is not blocked,
-		// or the account has been hidden via hideuser.
-		if( $pager->getNumRows() > 0 ) {
-			$pager->mLimit = 1; # Show only latest log entry.
-			$wgOut->addHTML( '<div class="mw-warning-with-logexcerpt">' );
-			$wgOut->addWikiMsg( 'sp-contributions-blocked-notice' );
-			$wgOut->addHTML(
-				$loglist->beginLogEventsList() .
-				$pager->getBody() .
-				$loglist->endLogEventsList()
-			);
-			if( $pager->getNumRows() > $pager->mLimit ) {
-				$wgOut->addHTML( $wgUser->getSkin()->link(
-					SpecialPage::getTitleFor( 'Log', 'block' ),
-					wfMsgHtml( 'log-fulllog' ),
-					array(),
-					array( 'page' => $title->getPrefixedText() )
-				) );
-			}
-			$wgOut->addHTML( '</div>' );
-		}
-	}
 	/**
 	 * Generates the namespace selector form with hidden attributes.
 	 * @param $this->opts Array: the options to be included.

languages/messages/MessagesEn.php
@@ -1253,6 +1253,7 @@ or <span class="plainlinks">[{{fullurl:{{#Special:Log}}|page={{urlencode:{{FULLP
 'noarticletextanon'                => '{{int:noarticletext}}', # do not translate or duplicate this message to other languages
 'userpage-userdoesnotexist'        => 'User account "$1" is not registered.
 Please check if you want to create/edit this page.',
+'userpage-userdoesnotexist-view'        => 'User account "$1" is not registered.',
 'clearyourcache'                   => "'''Note - After saving, you may have to bypass your browser's cache to see the changes.'''
 '''Mozilla / Firefox / Safari:''' hold ''Shift'' while clicking ''Reload'', or press either ''Ctrl-F5'' or ''Ctrl-R'' (''Command-R'' on a Macintosh);
 '''Konqueror: '''click ''Reload'' or press ''F5'';
@@ -1341,6 +1342,8 @@ You should consider whether it is appropriate to continue editing this page.
 The deletion and move log for this page are provided here for convenience:",
 'moveddeleted-notice'              => 'This page has been deleted.
 The deletion and move log for the page are provided below for reference.',
+'renamed-notice'                   => 'This user has been renamed.
+The rename log is provided below for reference.',
 'log-fulllog'                      => 'View full log',
 'edit-hook-aborted'                => 'Edit aborted by hook.
 It gave no explanation.',

maintenance/language/messages.inc
@@ -540,6 +540,7 @@ $wgMessageStructure = array(
 		'noarticletext-nopermission',
 		'noarticletextanon',
 		'userpage-userdoesnotexist',
+		'userpage-userdoesnotexist-view',
 		'clearyourcache',
 		'usercssyoucanpreview',
 		'userjsyoucanpreview',
@@ -588,6 +589,7 @@ $wgMessageStructure = array(
 		'permissionserrorstext-withaction',
 		'recreate-moveddeleted-warn',
 		'moveddeleted-notice',
+		'renamed-notice',
 		'log-fulllog',
 		'edit-hook-aborted',
 		'edit-gone-missing',

RELEASE-NOTES
@@ -222,6 +222,8 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
   excerpt from the block log.
 * (bug 19646) New hook: ImgAuthBeforeStream for tests and functionality before
   file is streamed to user, but only when using img_auth
+* Note on non-existing user and user talk pages if user does not exist and show
+  renameuser log if the user has been renamed
 
 === Bug fixes in 1.16 ===
 

includes/OutputPage.php
@@ -2115,4 +2115,55 @@ class OutputPage {
 		}
 		$this->addHTML( $this->parse( $s, /*linestart*/true, /*uilang*/true ) );
 	}
+	/*
+	 * Show log excerpt. All parameters are optional 
+	 * (but it makes sense to define at least $page OR $user)
+	 * @param String $page Page title for filtering the log
+	 * @param String $user Username for filtering the log
+	 * @param String $logtypes Log types, like delete, move, etc.
+	 * 	May be a single log type or an array of log types
+	 * @param int $numEntries Number of log entries to show at maximum (default: 10)
+	 * 	If there are too many entries, a "View full log" link is displayed
+	 * @return boolean Returns true if there was something in the log
+	 */
+	public function showLogs( $page = '', $user = '', $logtypes = array(), $msgKey = '' , $numEntries = 10 ) {
+		global $wgUser, $wgOut;
+		$loglist = new LogEventsList( $wgUser->getSkin(), $wgOut );
+		$pager = new LogPager( $loglist, $logtypes, $user, $page);
+		if( $pager->getNumRows() > 0 ) {
+			$pager->mLimit = $numEntries;
+			$wgOut->addHTML( '<div class="mw-warning-with-logexcerpt">' );
+			if ( $msgKey )
+				$wgOut->addWikiMsg( $msgKey );
+			$wgOut->addHTML(
+				$loglist->beginLogEventsList() .
+				$pager->getBody() .
+				$loglist->endLogEventsList()
+			);
+			if( $pager->getNumRows() > $numEntries ) { # Show "Full log" link
+				$urlParam = array();
+				if ( $page != '')
+					$urlParam['page'] = $page;
+				if ( $user != '')
+					$urlParam['user'] = $user;
+				if ( !is_array( $logtypes ) ) # Make it an array, if it isn't
+					$logtypes = array( $logtypes );
+				# If there is exactly one log type, we can link to Special:Log/type
+				if ( count( $logtypes ) == 1 )
+					$urlParam['type'] = $logtypes[0];
+				$wgOut->addHTML( $wgUser->getSkin()->link(
+					SpecialPage::getTitleFor( 'Log' ),
+					wfMsgHtml( 'log-fulllog' ),
+					array(),
+					$urlParam
+				) );
+
+			}
+			$wgOut->addHTML( '</div>' );
+			return true;
+		}
+		return false;
+	}
+
+
 }

commit 21cdfc19729cd9182caa3a4517aeade539046235

Follow-up on r56284: LogEventsList::showLogExtract gets associative array for additional parameters. Adjusted all calls that use additional parameters. Also improved Special:Blockip, which now uses those new parameters instead of using own functions. Fixed HistoryPage which was broken with r56251.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@56420 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/Article.php
@@ -1209,19 +1209,14 @@ class Article {
 				$wgOut->wrapWikiMsg( '<div class="mw-userpage-userdoesnotexist error">$1</div>',
 					array( 'userpage-userdoesnotexist-view', $this->mTitle->getBaseText() ) );
 			}
-
 		}
 		wfRunHooks( 'ShowMissingArticle', array( $this ) );
 		# Show delete and move logs
-		LogEventsList::showLogExtract(
-			$wgOut,
-			array( 'delete', 'move' ), 
-			$this->mTitle->getPrefixedText(),
-			'',
-			10,
-			array( "log_action != 'revision'" ),
-			false,
-			array( 'moveddeleted-notice' )
+		LogEventsList::showLogExtract( $wgOut, array( 'delete', 'move' ), $this->mTitle->getPrefixedText(), '',
+			array(  'lim' => 10,
+				'conds' => array( "log_action != 'revision'" ),
+				'showIfEmpty' => false,
+				'msgKey' => array( 'moveddeleted-notice' ) ) 
 		);
 
 		# Show error message

includes/EditPage.php
@@ -730,8 +730,12 @@ class EditPage {
 		}
 		# Give a notice if the user is editing a deleted/moved page...
 		if ( !$this->mTitle->exists() ) {
-			LogEventsList::showLogExtract( $wgOut, array( 'delete', 'move' ), 
-				$this->mTitle->getPrefixedText(), '', 10, array( "log_action != 'revision'" ), false, 'recreate-moveddeleted-warn');
+			LogEventsList::showLogExtract( $wgOut, array( 'delete', 'move' ), $this->mTitle->getPrefixedText(), 
+				'', array( 'lim' => 10, 
+					   'conds' => array( "log_action != 'revision'" ), 
+					   'showIfEmpty' => false, 
+					   'msgKey' => array( 'recreate-moveddeleted-warn') ) 
+			);
 		}
 	}
 
@@ -1278,10 +1282,8 @@ class EditPage {
 				$noticeMsg = 'protectedpagewarning';
 				$classes[] = 'mw-textarea-protected';
 			}
-			$wgOut->addHTML( "<div class='mw-warning-with-logexcerpt'>\n" );
-			$wgOut->addWikiMsg( $noticeMsg );
-			LogEventsList::showLogExtract( $wgOut, 'protect', $this->mTitle->getPrefixedText(), '', 1 );
-			$wgOut->addHTML( "</div>\n" );
+			LogEventsList::showLogExtract( $wgOut, 'protect', $this->mTitle->getPrefixedText(), '', 
+				array( 'lim' => 1, 'msgKey' => array( $noticeMsg ) ) );
 		}
 		if ( $this->mTitle->isCascadeProtected() ) {
 			# Is this page under cascading protection from some source pages?

includes/HistoryPage.php
@@ -105,7 +105,13 @@ class HistoryPage {
 		 */
 		if( !$this->title->exists() ) {
 			$wgOut->addWikiMsg( 'nohistory' );
-			$this->article->showLogs(); // show deletion/move log if there is an entry
+			# show deletion/move log if there is an entry
+			LogEventsList::showLogExtract( $wgOut, array( 'delete', 'move' ), $this->title->getPrefixedText(), '',
+				array(  'lim' => 10,
+					'conds' => array( "log_action != 'revision'" ),
+					'showIfEmpty' => false,
+					'msgKey' => array( 'moveddeleted-notice' ) ) 
+			);
 			wfProfileOut( __METHOD__ );
 			return;
 		}

includes/LogEventsList.php
@@ -571,26 +571,36 @@ class LogEventsList {
 	 * @param $types String or Array
 	 * @param $page String The page title to show log entries for
 	 * @param $user String The user who made the log entries
-	 * @param $lim Integer Limit of items to show, default is 50
-	 * @param $conds Array Extra conditions for the query
-	 * @param $showIfEmpty boolean Set to false if you don't want any output in case the loglist is empty
+	 * @param $param Associative Array with the following additional options:
+	 * 	lim Integer Limit of items to show, default is 50
+	 * 	conds Array Extra conditions for the query (e.g. "log_action != 'revision'")
+	 *	showIfEmpty boolean Set to false if you don't want any output in case the loglist is empty
 	 * 		if set to true (default), "No matching items in log" is displayed if loglist is empty
-	 * @param $msgKey Array If you want a nice box with a message, set this
+	 * 	msgKey Array If you want a nice box with a message, set this
 	 *              to the key of the message. First element is the message
 	 *              key, additional optional elements are parameters for the
 	 *              key that are processed with wgMsgExt and option 'parse'
 	 * @return Integer Number of total log items (not limited by $lim)
 	 */
-	public static function showLogExtract( &$out, $types=array(), $page='', $user='', $lim=0, $conds=array(), $showIfEmpty = true, $msgKey = array() ) {
+	public static function showLogExtract( &$out, $types=array(), $page='', $user='', 
+			$param = array( 'lim' => 0, 'conds' => array(), 'showIfEmpty' => true, 'msgKey' => array('') ) ) {
+
 		global $wgUser, $wgOut;
-		# Insert list of top 50 or so items
+		# Convert $param array to individual variables
+		$lim = $param['lim'];
+		$conds = $param['conds'];
+		$showIfEmpty = $param['showIfEmpty'];
+		$msgKey = $param['msgKey'];
+		if ( !(is_array($msgKey)) )
+			$msgKey = array( $msgKey );
+		# Insert list of top 50 (or top $lim) items
 		$loglist = new LogEventsList( $wgUser->getSkin(), $wgOut, 0 );
 		$pager = new LogPager( $loglist, $types, $user, $page, '', $conds );
 		if( $lim > 0 ) $pager->mLimit = $lim;
 		$logBody = $pager->getBody();
 		$s = '';
 		if( $logBody ) {
-			if ( $msgKey ) {
+			if ( $msgKey[0] ) {
 				$s = '<div class="mw-warning-with-logexcerpt">';
 
 				if ( count( $msgKey ) == 1 ) {
@@ -627,7 +637,7 @@ class LogEventsList {
 			);
 
 		}
-		if ( $logBody && $msgKey )
+		if ( $logBody && $msgKey[0] )
 			$s .= '</div>';
 
 		if( $out instanceof OutputPage ){

includes/specials/SpecialBlockip.php
@@ -585,22 +585,13 @@ class IPBlockForm {
 
 	private function showLogFragment( $out, $title ) {
 		global $wgUser;
-		$out->addHTML( Xml::element( 'h2', NULL, LogPage::logName( 'block' ) ) );
-		$count = LogEventsList::showLogExtract( $out, 'block', $title->getPrefixedText(), '', 10 );
-		if( $count > 10 ) {
-			$out->addHTML( $wgUser->getSkin()->link(
-				SpecialPage::getTitleFor( 'Log' ),
-				wfMsgHtml( 'blocklog-fulllog' ),
-				array(),
-				array(
-					'type' => 'block',
-					'page' => $title->getPrefixedText() ) ) );
-		}
+		LogEventsList::showLogExtract( $out, 'block', $title->getPrefixedText(), '', 
+			array( 'lim' => 10, 'msgKey' => array( 'blocklog-showlog' ) ) );
 		// Add suppression block entries if allowed
-		if( $wgUser->isAllowed('hideuser') ) {
-			$out->addHTML( Xml::element( 'h2', NULL, LogPage::logName( 'suppress' ) ) );
+		if( $wgUser->isAllowed( 'hideuser' ) ) {
 			LogEventsList::showLogExtract( $out, 'suppress', $title->getPrefixedText(), '',
-				10, array('log_action' => array('block','reblock','unblock')) );
+				array('lim' => 10, 'conds' => array('log_action' => array('block','reblock','unblock')), 
+				'msgKey' => array( 'blocklog-showsuppresslog' ) ) );
 		}
 	}
 

includes/specials/SpecialContributions.php
@@ -226,8 +226,8 @@ class SpecialContributions extends SpecialPage {
 
 			// Show a note if the user is blocked and display the last block log entry.
 			if ( User::newFromID( $id )->isBlocked() )
-				LogEventsList::showLogExtract( $wgOut, 'block', $nt->getPrefixedText(), '', 1, 
-					array(), false, 'sp-contributions-blocked-notice' );
+				LogEventsList::showLogExtract( $wgOut, 'block', $nt->getPrefixedText(), '',
+					array( 'lim' => 1, 'showIfEmpty' => false, 'msgKey' => array( 'sp-contributions-blocked-notice' ) ) );
 		}
 	
 		// Old message 'contribsub' had one parameter, but that doesn't work for

includes/specials/SpecialMovepage.php
@@ -180,7 +180,7 @@ class MovePageForm {
 			}
 			$wgOut->addHTML( "<div class='mw-warning-with-logexcerpt'>\n" );
 			$wgOut->addWikiMsg( $noticeMsg );
-			LogEventsList::showLogExtract( $wgOut, 'protect', $this->oldTitle->getPrefixedText(), '', 1 );
+			LogEventsList::showLogExtract( $wgOut, 'protect', $this->oldTitle->getPrefixedText(), '', array( 'lim' => 1 ) );
 			$wgOut->addHTML( "</div>\n" );
 		}
 

includes/specials/SpecialRevisiondelete.php
@@ -184,12 +184,12 @@ class SpecialRevisionDelete extends UnlistedSpecialPage {
 		# Show relevant lines from the deletion log
 		$wgOut->addHTML( "<h2>" . htmlspecialchars( LogPage::logName( 'delete' ) ) . "</h2>\n" );
 		LogEventsList::showLogExtract( $wgOut, 'delete',
-			$this->targetObj->getPrefixedText(), '', 25, $qc );
+			$this->targetObj->getPrefixedText(), '', array( 'lim' => 25, 'conds' => $qc ) );
 		# Show relevant lines from the suppression log
 		if( $wgUser->isAllowed( 'suppressionlog' ) ) {
 			$wgOut->addHTML( "<h2>" . htmlspecialchars( LogPage::logName( 'suppress' ) ) . "</h2>\n" );
 			LogEventsList::showLogExtract( $wgOut, 'suppress',
-				$this->targetObj->getPrefixedText(), '', 25, $qc );
+				$this->targetObj->getPrefixedText(), '', array( 'lim' => 25, 'conds' => $qc ) );
 		}
 	}
 

languages/messages/MessagesEn.php
@@ -2945,7 +2945,8 @@ See [[Special:IPBlockList|IP block list]] to review blocks.',
 'autoblocker'                     => 'Autoblocked because your IP address has been recently used by "[[User:$1|$1]]".
 The reason given for $1\'s block is: "$2"',
 'blocklogpage'                    => 'Block log',
-'blocklog-fulllog'                => 'Full block log',
+'blocklog-showlog'                => 'This user has been blocked previously. The block log is provided below for reference:',
+'blocklog-showsuppresslog'        => 'This user has been blocked and hidden previously. The suppress log is provided below for reference:',
 'blocklogentry'                   => 'blocked [[$1]] with an expiry time of $2 $3',
 'reblock-logentry'                => 'changed block settings for [[$1]] with an expiry time of $2 $3',
 'blocklogtext'                    => 'This is a log of user blocking and unblocking actions.

maintenance/language/messages.inc
@@ -1987,7 +1987,8 @@ $wgMessageStructure = array(
 		'contribslink',
 		'autoblocker',
 		'blocklogpage',
-		'blocklog-fulllog',
+		'blocklog-showlog',
+		'blocklog-showsuppresslog',
 		'blocklogentry',
 		'reblock-logentry',
 		'blocklogtext',