ezc/mediawiki

commit 28e173775e3952676d11c33d770554f67cfdb7bd

(bug 28864) Fix UtfNormal benchmark & test case runners from regression in r85327, r85944
It's possible that this has some side effect on HipHop builds -- the require_once on UtfNormalDefines.php that had been moved from UtfNormalUtil.php to MediaWiki's Setup.php in r85944 had been previously damaged by r85327 sticking a MediaWiki-specific MWInit class static method call into it, making it break on all the generator & test & benchmark code which does not depend on MediaWiki.
With the require_once restored everything seems to work, though it's possible that it ends up trying to include twice on HipHop -- have not tested.





git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@87748 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/normal/UtfNormalUtil.php
@@ -25,6 +25,9 @@
  * @ingroup UtfNormal
  */
 
+/** */
+require_once dirname(__FILE__).'/UtfNormalDefines.php';
+
 /**
  * Return UTF-8 sequence for a given Unicode code point.
  * May die if fed out of range data.

commit 092dff491ac7fd5e6685613a34628e3df3a1d684

Fix for HipHop breakage in r87748 etc. Please do not use require_once(dirname(__FILE__)...) to fetch class, function or define() dependencies anymore. You do not need to have HipHop to know that doing so will break it.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@88177 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/GlobalFunctions.php
@@ -8,10 +8,6 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( "This file is part of MediaWiki, it is not a valid entry point" );
 }
 
-if ( !defined( 'MW_COMPILED' ) ) {
-	require_once( dirname( __FILE__ ) . '/normal/UtfNormalUtil.php' );
-}
-
 // Hide compatibility functions from Doxygen
 /// @cond
 

includes/Setup.php
@@ -326,6 +326,7 @@ if ( !defined( 'MW_COMPILED' ) ) {
 	wfProfileOut( $fname . '-exception' );
 
 	wfProfileIn( $fname . '-includes' );
+	require_once( "$IP/includes/normal/UtfNormalUtil.php" );
 	require_once( "$IP/includes/GlobalFunctions.php" );
 	require_once( "$IP/includes/Hooks.php" );
 	require_once( "$IP/includes/ProxyTools.php" );

includes/normal/Utf8CaseGenerate.php
@@ -29,6 +29,7 @@ if( php_sapi_name() != 'cli' ) {
 	die( "Run me from the command line please.\n" );
 }
 
+require_once 'UtfNormalDefines.php';
 require_once 'UtfNormalUtil.php';
 
 $in = fopen("UnicodeData.txt", "rt" );

includes/normal/Utf8Test.php
@@ -26,6 +26,8 @@
  */
 
 /** */
+
+require_once 'UtfNormalDefines.php';
 require_once 'UtfNormalUtil.php';
 require_once 'UtfNormal.php';
 mb_internal_encoding( "utf-8" );

includes/normal/UtfNormal.php
@@ -28,8 +28,6 @@
  * @defgroup UtfNormal UtfNormal
  */
 
-require_once dirname(__FILE__).'/UtfNormalUtil.php';
-
 /**
  * For using the ICU wrapper
  */

includes/normal/UtfNormalBench.php
@@ -28,6 +28,7 @@ if( isset( $_SERVER['argv'] ) && in_array( '--icu', $_SERVER['argv'] ) ) {
 	dl( 'php_utfnormal.so' );
 }
 
+require_once 'UtfNormalDefines.php';
 require_once 'UtfNormalUtil.php';
 require_once 'UtfNormal.php';
 

includes/normal/UtfNormalGenerate.php
@@ -29,6 +29,7 @@ if( php_sapi_name() != 'cli' ) {
 	die( "Run me from the command line please.\n" );
 }
 
+require_once 'UtfNormalDefines.php';
 require_once 'UtfNormalUtil.php';
 
 $in = fopen("DerivedNormalizationProps.txt", "rt" );

includes/normal/UtfNormalMemStress.php
@@ -30,6 +30,7 @@ if( isset( $_SERVER['argv'] ) && in_array( '--icu', $_SERVER['argv'] ) ) {
 	dl( 'php_utfnormal.so' );
 }
 
+require_once 'UtfNormalDefines.php';
 require_once 'UtfNormalUtil.php';
 require_once 'UtfNormal.php';
 

includes/normal/UtfNormalUtil.php
@@ -25,9 +25,6 @@
  * @ingroup UtfNormal
  */
 
-/** */
-require_once dirname(__FILE__).'/UtfNormalDefines.php';
-
 /**
  * Return UTF-8 sequence for a given Unicode code point.
  * May die if fed out of range data.

includes/normal/UtfNormalTest.php
@@ -49,6 +49,7 @@ if( isset( $_SERVER['argv'] ) && in_array( '--icu', $_SERVER['argv'] ) ) {
 	dl( 'php_utfnormal.so' );
 }
 
+require_once 'UtfNormalDefines.php';
 require_once 'UtfNormalUtil.php';
 require_once 'UtfNormal.php';
 