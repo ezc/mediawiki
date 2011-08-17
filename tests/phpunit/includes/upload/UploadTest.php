ezc/mediawiki

commit 4b6c3844ca6488ab8aef3f9de3ce96d4e0045780

follow-up r88171 - fix fatal if suggested filename has illegal character in it.
Check to make sure the title object actual exists, before checking its namespace,
since alll the does this have illegal chars check is done after the does it
start with file: check.

For future note though, when using upload by url feature, the suggested
destination name when you put http://upload.wikimedia.org/wikipedia/commons/e/e0/Petrorhagia_prolifera_%281%29.JPG is illegal, which is probably not very user-friendly (filed as bug 30390)


git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94601 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/upload/UploadBase.php
@@ -611,7 +611,7 @@ abstract class UploadBase {
 		 * and that the namespace prefix needs to be stripped of.
 		 */
 		$title = Title::newFromText( $this->mDesiredDestName );
-		if ( $title->getNamespace() == NS_FILE ) {
+		if ( $title && $title->getNamespace() == NS_FILE ) {
 			$this->mFilteredName = $title->getDBkey();
 		} else {
 			$this->mFilteredName = $this->mDesiredDestName;

commit 1c010f6bab171814232ae0ca7f10560067e077d1

follow-up r94601 - add unit tests for stripping the File: prefix from upload, and test handling illegal characters in upload title (thing that caused the issue in r94601)
(I have no idea if things like this (unit-tests) should be tagged 1.18, I assume no but am unsure)


git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk@94728 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

tests/phpunit/includes/upload/UploadTest.php
@@ -41,6 +41,16 @@ class UploadTest extends MediaWikiTestCase {
 			'A-B.jpg', UploadBase::OK,
 			'upload title with colon' );
 
+		/* Stripping leading File: prefix */
+		$this->assertUploadTitleAndCode( 'File:C.jpg',
+			'C.jpg', UploadBase::OK,
+			'upload title with File prefix' );
+
+		/* Test illegal suggested title (r94601) */
+		$this->assertUploadTitleAndCode( '%281%29.JPG',
+			null, UploadBase::ILLEGAL_FILENAME,
+			'illegal title for upload' );
+
 		/* A title without extension */
 		$this->assertUploadTitleAndCode( 'A',
 			null, UploadBase::FILETYPE_MISSING,